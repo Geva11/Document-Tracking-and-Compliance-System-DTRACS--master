b30b84d620203d1389f0a314cca3a70906b9ec73
Geva, Nino Mynel A. <ninomynelageva@iskolarngbayan.edu.gov.ph>
2025-08-20 16:48:15 +0800

    feat: Add new pages and components for SGOD sections including DRRM, Education Facilities, HRD, Planning Research, SMME, SMN, School Health, and Youth Formation. Implement styles and structure for each section. Enhance the StartUp page with background and navigation. Update Todo app with Completed, Past Due, Upcoming, and Task Detail functionalities and styles.

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import StartUp from "./pages/StartUp/StartUp";
+import Login from "./pages/Login/Login";
+import RegisterSchool from "./pages/Regsiter/RegisterSchool";
+import RegisterOffice from "./pages/Regsiter/RegisterOffice";
+import SchoolHome from "./pages/Home/SchoolHome";
+import OfficeHome from "./pages/Home/OfficeHome";
+import Todo from "./pages/Todo/Upcoming/Upcoming";
+import PastDue from "./pages/Todo/PastDue/PastDue";
+import Completed from "./pages/Todo/Completed/Completed";
+import TaskDetail from "./pages/Todo/TaskDetail/TaskDetail";
+import OfficeManageAccount from "./pages/ManageAccount/OfficeManageAccount";
+import SchoolManageAccount from "./pages/ManageAccount/SchoolManageAccount";
+import SchoolDashboard from "./pages/Dashboard/SchoolDashboard";
+import SGOD from "./pages/SGOD/SGOD";
+import PlanningResearch from "./pages/SGOD/PlanningResearch/PlanningResearch";
+import SMME from "./pages/SGOD/SMME/SMME";
+import HRD from "./pages/SGOD/HRD/HRD";
+import SchoolHealth from "./pages/SGOD/SchoolHealth/SchoolHealth";
+import EducationFacilities from "./pages/SGOD/EducationFacilities/EducationFacilities";
+import SMN from "./pages/SGOD/SMN/SMN";
+import YouthFormation from "./pages/SGOD/YouthFormation/YouthFormation";
+import Schools from "./pages/School/School";
+import OfficeDashboard from "./pages/Dashboard/OfficeDashboard";
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <Routes location={location} key={location.pathname}>
+      {/* Public routes */}
+      <Route path="/" element={<StartUp />} />
+      <Route path="/login/school" element={<Login />} />
+      <Route path="/login/office" element={<Login />} />
+      <Route path="/register/school" element={<RegisterSchool />} />
+      <Route path="/register/office" element={<RegisterOffice />} />
+
+      {/* Protected layout: Home + Sidebar nav */}
+      <Route element={<SchoolHome />}>
+        <Route path="/home" element={<SchoolDashboard />} />
+        <Route path="/SGOD" element={<SGOD />} />
+        <Route path="/SGOD/SMME" element={<SMME />} />
+        <Route path="/SGOD/planning-research" element={<PlanningResearch />} />
+        <Route path="/SGOD/HRD" element={<HRD />} />
+        <Route path="/SGOD/school-health" element={<SchoolHealth />} />
+        <Route path="/SGOD/education-facilities" element={<EducationFacilities />} />
+        <Route path="/SGOD/SMN" element={<SMN />} />
+        <Route path="/SGOD/DRRM" element={<SMN />} />
+        <Route path="/SGOD/youth-formation" element={<YouthFormation />} />
+        
+         {/* Todo routes */}
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/todo/upcoming" element={<Todo />} />
+        <Route path="/todo/past-due" element={<PastDue />} />
+        <Route path="/todo/completed" element={<Completed />} />
+        <Route path="/todo/task/:id" element={<TaskDetail />} />
+
+        <Route path="/s-manage-account" element={<SchoolManageAccount />} />
+      </Route>
+
+      <Route element={<OfficeHome />}>
+        <Route path="/task" element={<OfficeDashboard />} />
+        <Route path="/schools" element={<Schools />} />
+        <Route path="/o-manage-account" element={<OfficeManageAccount />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/SGOD/SGOD.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./SGOD.css";
+import SectionCard from "../../components/SectionCard/SectionCard";
+import NoImage from "../../assets/images/no-image.png"; // Placeholder image
+import Image1 from "../../assets/images/Planning-And-Research.png";
+import { useOutletContext, useNavigate } from "react-router-dom"; // 👈 get context from Home
+
+const SGOD = () => {
+  const { isExpanded } = useOutletContext();
+  const navigate = useNavigate();
+
+  const sections = [
+    { title: "School Management Monitoring and Evaluation Section", path: "SMME",image: NoImage },
+    { title: "Planning and Research", path: "planning-research", image: Image1 },
+    { title: "Human Resource Development Section", path: "HRD", image: NoImage },
+    { title: "School Health Section", path: "school-health", image: NoImage },
+    { title: "Education Facilities Section", path: "education-facilities", image: NoImage },
+    { title: "Social Mobilization and Networking Section", path: "SMN", image: NoImage },
+    { title: "Disaster Risk Reduction and Management Unit", path: "DRRM", image: NoImage },
+    { title: "Youth Formation Section", path: "youth-formation", image: NoImage },
+  ];
+
+  return (
+    <div className={`section-container ${isExpanded ? "expanded" : "collapsed"}`}>
+      {sections.map((section, index) => (
+        <SectionCard
+          key={index}
+          title={section.title}
+          image={section.image}
+          onClick={() => section.path && navigate(section.path, { relative: "path" })}
+        />
+      ))}
+    </div>
+  );
+};
+
+
+export default SGOD;
